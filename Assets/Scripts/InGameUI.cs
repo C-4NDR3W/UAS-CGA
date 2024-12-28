652fb0d10545c9606cfbe4824d39af5b5d4330ac
Andrew <clarenceandrew9105@gmail.com>
2024-12-28 11:45:17 +0700

    Menu done except options and save(?)

--- Assets/Scripts/InGameUI.cs
+++ Assets/Scripts/InGameUI.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class InGameUI : MonoBehaviour
 {
     public static InGameUI Instance;
+    public GameObject menuUIPanel;
     public GameObject battleUIPanel;
     public GameObject doctorUIPanel;
 
@@ -19,5 +22,18 @@ public class InGameUI : MonoBehaviour
         {
             Destroy(gameObject); 
         }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            menuUIPanel.SetActive(!menuUIPanel.activeSelf);
+        }
+    }
+    public void OnMainMenuButtonPressed()
+    {
+        Debug.Log("Main Menu");
+        SceneManager.LoadScene(0);
     }
 }
